38003129d44f31c4a197fd7871321655dcda7bc2
Thien-Thi Nguyen <ttn@gnuvola.org>
2022-04-24 09:25:12 -0400

    add check for required arg TEMPLATE
    * format.sh
      (version): Bump to "1.2".
      <top-level>: Handle missing arg TEMPLATE.

--- format.sh
+++ format.sh
@@ -6,7 +6,8 @@
 ##
 me=$(basename $0)
 
-version='1.1'
+version='1.2'
+# 1.2  -- add check for required arg TEMPLATE
 # 1.1  -- add --help/--version support
 # 1.0  -- initial release
 
@@ -18,6 +19,11 @@ fi
 if [ x"$1" = x--version ] ; then
     echo $me '(gana)' $version
     exit 0
+fi
+
+if [ x"$1" = x ] ; then
+    echo >&2 "$me: ERROR: missing arg TEMPLATE (try --help)"
+    exit 1
 fi
 
 template="$1"
