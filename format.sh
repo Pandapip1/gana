f99c5dd2c488f132024b38aa71b0be18ab4b6fd3
Thien-Thi Nguyen <ttn@gnuvola.org>
2022-04-24 09:17:30 -0400

    add --help/--version support
    * format.sh
      (me, version): New vars.
      <top-level>: Handle ‘--help’ on command line.
      <top-level>: Likewise for ‘--version’.
      (template): New var.
      <top-level>: Use ‘recfmt -f’ and ‘$template’.

--- format.sh
+++ format.sh
@@ -1,2 +1,27 @@
 #!/bin/sh
-exec recfmt "`cat $1`"
+##
+# Usage: format.sh TEMPLATE
+#
+# This runs recfmt w/ template file TEMPLATE on stdin, writing to stdout.
+##
+me=$(basename $0)
+
+version='1.1'
+# 1.1  -- add --help/--version support
+# 1.0  -- initial release
+
+if [ x"$1" = x--help ] ; then
+    sed '/^##/,/^##/!d;/^##/d;s/^# //g;s/^#$//g' $0
+    exit 0
+fi
+
+if [ x"$1" = x--version ] ; then
+    echo $me '(gana)' $version
+    exit 0
+fi
+
+template="$1"
+
+exec recfmt -f "$template"
+
+# format.sh ends here
